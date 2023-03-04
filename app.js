db5427ec3ba289c0fbd6738cdf3ffa5b8e5ef44d
gyan <gyan9427@gmail.com>
2023-03-04 20:37:38 +0530

    create home template with post route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
