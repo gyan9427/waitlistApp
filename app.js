4c4044e229c6de25bcd21dc48551209630a148b2
gyan <gyan9427@gmail.com>
2023-03-04 19:11:55 +0530

    initial app setup and view setup

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("nodemon running")
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json());
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(8000,()=>{
+    console.log('listening to PORT: 8000....')
+})
